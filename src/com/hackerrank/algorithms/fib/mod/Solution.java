7b2edd0efa78dd86d6db1ab457ef94de8941886d
Darek <dblanken@localhost.localdomain>
2017-05-15 16:48:50 -0700

    Figured I should add the challenge

--- src/com/hackerrank/algorithms/fib/mod/Solution.java
+++ src/com/hackerrank/algorithms/fib/mod/Solution.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 
+/**
+ * https://www.hackerrank.com/challenges/fibonacci-modified
+ * 
+ * @author blankd
+ */
 public class Solution {
   private static final Integer TWO = Integer.valueOf(2);
   private static final Integer F_ONE_IND = Integer.valueOf(0);
